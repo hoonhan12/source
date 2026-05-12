c3eb5c37bdeed5c4a0377b4b84dc3fe4261187c0
hoonhanaf <hoonhanaf@gmail.com>
2026-05-12 17:58:45 +0900

    파이썬 기초 문법

---
+++ pythonsource/hello.py
@@ -0,0 +1,11 @@
+print("Hello")
+
+# 줄단위 실행 => 특정 행에서 실행 오류가 나는 경우 프로그램은 멈추게 됨
+# 파이썬 자료형
+# 정수형, 문자형, 불린형, 리스트, 튜플, 딕셔너리
+
+# 변수 : 프로그램 내부에서 값을 담아놓기 위한 공간(이름 사용 필수)
+
+# \n == enter
+multiline = "life is too short\nYou need python"
+print(multiline)
